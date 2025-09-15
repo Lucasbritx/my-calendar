b2f97a93d1d7366d0dbd2e3a22898669632eff9c
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-09-15 11:10:16 -0300

    feat: created meeting type

---
+++ src/types/meeting.ts
@@ -0,0 +1,9 @@
+export type IMeeting = {
+    id: number;
+    title: string;
+    startTime: number;
+    endTime: number;
+    formattedDate: string;
+    formattedStartTime: string;
+    formattedEndTime: string;
+};
